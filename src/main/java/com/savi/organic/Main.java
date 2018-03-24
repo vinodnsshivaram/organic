17b16d05a2467b81001ead0e5b433a9842f1daa7
vinodnsshivaram <vinodnsshivaram@gmail.com>
2018-03-23 21:47:57 -0600

    added host properties

--- src/main/java/com/savi/organic/Main.java
+++ src/main/java/com/savi/organic/Main.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Main implements CommandLineRunner {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
